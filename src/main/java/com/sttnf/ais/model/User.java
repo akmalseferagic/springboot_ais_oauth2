a27aa9805348250c33d66e7749792ad2e23684d9
akmalseferagic <akmalseferagic@gmail.com>
2021-07-22 15:21:15 +0700

    ubah log

--- src/main/java/com/sttnf/ais/model/User.java
+++ src/main/java/com/sttnf/ais/model/User.java
@@ -14,14 +14,19 @@ public class User {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     @Column(name = "ID")
     private long id;
+    
     @Column(name = "FIRST_NAME")
     private String firstName;
+    
     @Column(name = "LAST_NAME")
     private String lastName;
+    
     @Column(name = "USERNAME")
     private String username;
+    
     @Column(name = "PASSWORD")
     private String password;
+    
     @Column(name = "EMAIL")
     private String email;
 
